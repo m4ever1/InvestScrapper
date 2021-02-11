cf2bb8809180a5a359cb0f4aed070b7e9cd1cc72
m4ever1 <mglviegas@live.com.pt>
2021-02-11 02:10:23 +0000

    Changed to get stocks instead of fund comp.

--- main.py
+++ main.py
@@ -1,56 +1,20 @@
-import investpy
+import bs4 as bs
 import requests
-from investpy.utils.extra import random_user_agent
-import bs4
-import pandas
-import json
+import yfinance as yf
+import datetime
 
-df = investpy.get_funds_dict(country="Portugal")
 
-head = {
-    "User-Agent": random_user_agent(),
-    "X-Requested-With": "XMLHttpRequest",
-    "Accept": "text/html",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Connection": "keep-alive",
-}
-f = open("fundDict.json", "w")   
-fundsInfoDict = {}
-# get tables
-for fund in df:
-    print(fund["tag"])
-    tag = fund["tag"]    
-    url = "https://www.investing.com/funds/" + tag + "-holdings"
-    req = requests.get(url, headers=head)
-    soup = bs4.BeautifulSoup(req.text, "html.parser")
+resp = requests.get('http://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+soup = bs.BeautifulSoup(resp.text, 'lxml')
+table = soup.find('table', {'class': 'wikitable sortable'})
+tickers = []
+for row in table.findAll('tr')[1:]:
+    ticker = row.findAll('td')[0].text
+    tickers.append(ticker)
 
-    tables = soup.find_all("table", attrs={"class": "genTbl"})
-    print(len(tables))
-    getTable = [False, False, False, False]
-    if soup.find("div", attrs={"class":"asset section"}):
-        getTable[0] = True
-    if soup.find("div", attrs={"class" : "syleBox section"}):
-        getTable[1] = True
-    if soup.find(text="Sector Allocation"): #h3 Sector Allocation
-        getTable[2] = True
-    if soup.find(text="Top Holdings"):
-        getTable[3] = True
-    
-    finalStr = ""
-    for i, get in enumerate(getTable):
-        if get:
-            finalStr += str(tables[i])
-    if len(finalStr) != 0:
-        dfs = pandas.read_html(finalStr)
-        print(dfs)
-        for df in dfs:
-            csvF = open("./Funds/" + fund["name"] + ".csv", "a")
-            df.to_csv("./Funds/" + fund["name"] + ".csv", mode="a")
-            csvF.write("\n")
-            csvF.close()
-
-        fundsInfoDict[fund["name"]] = df.to_dict()
-
-json.dump(fundsInfoDict, f, sort_keys=True, indent=4)
-f.close()
-# styleBox = soup.find("table",attrs={"class": "boxBoard"})
+tickers = [s.replace('\n', '') for s in tickers]
+start = datetime.datetime(2005, 1, 1)
+end = datetime.datetime(2021, 1, 1)
+data = yf.download(tickers, start=start, end=end)
+data.to_csv('stocks.csv')
+print(data)
