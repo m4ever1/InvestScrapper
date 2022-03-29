d058da131eade7c79e90a8b977f27e2c44f9f420
mviegas <mviegas@infinera.com>
2022-03-29 22:18:46 +0100

    Finished X

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import yfinance as yf
 import datetime
 import pandas as pd
 import numpy as np
+from collections import defaultdict
 
 # store = pd.HDFStore('stocks.h5')
 
@@ -11,16 +12,16 @@ resp = requests.get('http://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
 soup = bs.BeautifulSoup(resp.text, 'lxml')
 table = soup.find('table', {'class': 'wikitable sortable'})
 tickers = []
-sectors_dict = {}
+sectors_dict = defaultdict(list)
 for row in table.findAll('tr')[1:]:
     ticker = row.findAll('td')[0].text
-    sector = row.findAll('td')[3].text
+    # sector = row.findAll('td')[4].text
     ticker = ticker.strip()
     tickers.append(ticker)
-    sectors_dict[ticker] = sector
+    # sectors_dict[sector].append(ticker)
 
 # tickers = [s.replace('\n', '') for s in tickers]
-# start = datetime.datetime(2017, 1, 1)
+# start = datetime.datetime(2011, 1, 1)
 # end = datetime.datetime(2021, 1, 1)
 # data = yf.download(tickers, start=start, end=end)
 
@@ -39,14 +40,14 @@ data.rename(columns = {"Unnamed: 0_level_0" : "Stat", "Unnamed: 0_level_1" : "Ti
 df = data["Adj Close"]
 df = df.dropna(how="all")
 df = df.dropna(axis=1)
-# df = df.interpolate()
+df = df.interpolate()
 G = pd.DataFrame()
 
 
 # Iterate through every ticker
 G = np.log(df).shift(-1) - np.log(df)
 
-G.dropna(how='all')
+G = G.dropna(how='all')
 
 # C = np.mean(G.multiply)
 
@@ -59,6 +60,56 @@ for ticker_i in G:
 
 C = pd.DataFrame(rows, columns=G.columns, index=G.columns)
 
+t = G.shape[0]
+n = G.shape[1]
 
-print(data)
+Q = t/n
+
+lMax = 1 + 1/Q + 2*np.sqrt(1/Q)
+lMin = 1 + 1/Q - 2*np.sqrt(1/Q)
+
+for row in table.findAll('tr')[1:]:
+    ticker = row.findAll('td')[0].text
+    sector = row.findAll('td')[3].text
+    ticker = ticker.strip()
+    if ticker in G.columns:
+        sectors_dict[sector].append(ticker)
+
+
+setOfSectors = set(sectors_dict.keys())
+
+Pmatrix = np.zeros([len(setOfSectors), len(G.columns)])
+P = pd.DataFrame(Pmatrix, columns=G.columns)
+
+
+
+for sectorId, stocks in enumerate(sectors_dict.values()):
+    for stock in stocks:
+        Nl = len(stocks)
+        P[stock][sectorId] = 1/Nl
+
+    
+
+w, v = np.linalg.eig(C)
+
+# eigVectors = np.delete(v, 0, axis=0)
+
+lastVector = 0
+for i, value in enumerate(w):
+    if value < lMax:
+        lastVector = i
+        break
+
+eigValues = w #[1:(lastVector)]
+eigVectors = v #[1:(lastVector)]
+
+eigVectorsSquared = np.square(eigVectors)
+
+X = np.dot(eigVectorsSquared, P.T)
+# X = np.zeros([442, 120])
+# for k, eigVector in enumerate(eigVectors):
+#     for l, pVector in enumerate(np.matrix(P)):
+#         X[k][l] = np.sum(np.multiply(pVector, np.square(eigVector)))
+
+print(X)
 
